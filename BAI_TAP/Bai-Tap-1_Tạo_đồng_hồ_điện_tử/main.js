83f77d2e99630a6244be5bd59c87ceff9bc6a73e
sonnd93 <sonjapan7@gmail.com>
2022-11-29 09:54:32 +0700

    a

--- BAI_TAP/Bai-Tap-1_Tạo_đồng_hồ_điện_tử/main.js
+++ BAI_TAP/Bai-Tap-1_Tạo_đồng_hồ_điện_tử/main.js
@@ -8,6 +8,7 @@ function Dong_ho() {
     gio.innerHTML = Gio_hien_tai;
     phut.innerHTML = Phut_hien_tai;
     giay.innerHTML = Giay_hien_tai;
+    Alarm();
 }
 var Dem_gio = setInterval(Dong_ho, 1000);
 
@@ -15,42 +16,40 @@ var img = [];
 var img_1 = "https://img.meta.com.vn/Data/image/2021/09/30/background-la-gi-anh-background-dep-4.jpg";
 var img_2 = "https://thuthuatnhanh.com/wp-content/uploads/2021/12/background-vu-tru-4K.jpg";
 var img_3 = "https://toigingiuvedep.vn/wp-content/uploads/2021/11/background-vu-tru-sang-tao.jpeg";
-var img_4 = "http://listsach.com/wp-content/uploads/2019/02/List-sach-hay-thien-van-hoc-vu-tru-va-cac-chom-sao.jpg";
+
 img.push(img_1);
 img.push(img_2);
 img.push(img_3);
-img.push(img_4);
+
 function getImg() {
     let body = document.querySelector('body')
     const ramdom = Math.floor(Math.random() * img.length)
     body.background = img[ramdom];
     body.style.backgroundsize = "100%";
 }
-setInterval(getImg, 10000);
+
+
 
 setTimeout(function () {
     document.getElementById("tictac").play(); // chay audio tictac
 }, 1)
 
 
-
-
-// setInterval(function () {
-//     document.getElementById("my_audio").play();
-// }, 1000)
-
-
-var myAudio = [];
-var myAudio_1 = "";
-var myAudio_2 = "";
-var myAudio_3 = "";
-var myAudio_4 = "";
-img.push(myAudio_1);
-img.push(myAudio_2);
-img.push(myAudio_3);
-img.push(myAudio_4);
-function getAudio() {
-    let = document.querySelector('getAudio')
-
+let audio = document.getElementById("my_audio");
+function Alarm() {
+    var Gio_hien_tai = new Date().getHours();
+    var Phut_hien_tai = new Date().getMinutes();
+    var Giay_hien_tai = new Date().getSeconds();
+    if (Gio_hien_tai == 9) {
+        if (Phut_hien_tai == 21) {
+            if (Giay_hien_tai == 40) {
+                audio.play();
+            }
+        }
+    }
 }
-setInterval(getAudio, 1000);
+
+
+
+
+setInterval(getImg, 10000);
